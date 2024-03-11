f4c672be27aeae6172b34362931f7b9905a3a259
GagoChad <gagika57@gmail.com>
2024-03-11 14:03:26 +0500

    Add CardList & Card Components

---
+++ src/components/CardList.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Card from './Card.vue'
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      :price="1205"
+      imgUrl="/sneakers/sneakers-1.jpg"
+    />
+  </div>
+</template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex justify-between border-b border-slay-300 px-10 py-8">
+  <header class="flex justify-between border-b border-slay-200 px-10 py-8">
     <div class="flex items-center gap-4">
       <img src="/logo.png" alt="Logo" class="w-10" />
       <div>
