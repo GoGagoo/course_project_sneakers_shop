49f554766a8b25a266f2a1cc56b2f8a04967fa09
Gagik Antonyan <gagika57@gmail.com>
2024-04-28 12:28:39 +0500

    Add Search Input & Selection of Sneakers Price Layouts

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,28 @@ import Header from './components/Header.vue'
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
 
-      <CardList />
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none" name="" id="">
+            <option value="">По названию</option>
+            <option value="">По дешевой цене</option>
+            <option value="">По дорогой цене</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" alt="" />
+            <input
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              placeholder="Поиск"
+              type="text"
+            />
+          </div>
+        </div>
+      </div>
     </div>
+
+    <CardList />
   </div>
 </template>
